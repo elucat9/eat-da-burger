2f860a71aa2b274334d577f16faa79b4936681c4
elucat9 <elucat9@gmail.com>
2019-03-16 09:52:17 -0700

    dammit jaws

--- server.js
+++ server.js
@@ -20,9 +20,10 @@ var mysql = require("mysql");
 
 connection = mysql.createConnection({
   host: 'thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-  user: '	kx6t8i8aqdlpk489',
+  user: 'kx6t8i8aqdlpk489',
   password: 'umz400fbpc6euvci',
-  database: 'tja1gja9o8kbckqx',
+  database: 'tja1gja9o8kbckqx'
+});
 
 connection.connect(function (err) {
   if (err) {
