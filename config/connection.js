0dc605f9a2bdc9b5d2333749cae23d9f09e4c5cc
elucat9 <elucat9@gmail.com>
2019-03-16 09:32:59 -0700

    jaws

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,9 @@ if (process.env.JAWS_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'burgers_db',
+        host: 'thh2lzgakldp794r.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: '	kx6t8i8aqdlpk489',
+        password: 'umz400fbpc6euvci',
+        database: 'tja1gja9o8kbckqx',
     })
 }
